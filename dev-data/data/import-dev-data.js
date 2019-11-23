832fce0703a13e7b72571771ef686a6a74b6f14c
Jorge Contreras <jorge.contreras789@gmail.com>
2019-11-23 13:35:59 -0500

    improve read performance with indexes

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const Tour = require('./../../models/tourModels');
+const Review = require('./../../models/reviewModel');
+const User = require('./../../models/userModel');
 dotenv.config({ path: './config.env' });
 
 const DB = process.env.DATABASE.replace(
@@ -23,10 +25,16 @@ mongoose
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
 );
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
 
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await Review.create(reviews);
+    await User.create(users, { validateBeforeSave: false });
     console.log('data successfully loaded!');
   } catch (err) {
     console.log(err);
@@ -37,6 +45,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('data successfully loaded!');
   } catch (err) {
     console.log(err);
