e4a4962ece352555a2c570a5d97d533d79b65acf
Jorge Contreras <jorge.contreras789@gmail.com>
2019-11-27 11:03:21 -0500

    prevent duplicate reviews

--- models/tourModels.js
+++ models/tourModels.js
@@ -36,7 +36,8 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       default: 4.5,
       min: [1, 'Rating must be between 1 and 5'],
-      max: [5, 'Rating must be between 1 and 5']
+      max: [5, 'Rating must be between 1 and 5'],
+      set: val => Math.round(val * 10) / 10
     },
     ratingsQuantity: {
       type: Number,
