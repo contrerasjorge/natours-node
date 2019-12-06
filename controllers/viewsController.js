a90b4a9939f2d064d7012c42b62f5c0ee99739b1
Jorge Contreras <jorge.contreras789@gmail.com>
2019-12-06 11:35:00 -0500

    Mapbox structure setup

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -17,7 +17,7 @@ exports.getTour = catchAsync(async (req, res) => {
   });
 
   res.status(200).render('tour', {
-    title: 'The Forest Hiker Tour',
+    title: `${tour.name} Tour`,
     tour
   });
 });
