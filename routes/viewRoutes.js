1754c4c9267f06c23da3030a27602f1b3c40f0e3
Jorge Contreras <jorge.contreras789@gmail.com>
2019-12-10 10:08:42 -0500

    Add the login screen

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const { getOverview, getTour } = require('../controllers/viewsController');
+const {
+  getOverview,
+  getTour,
+  getLoginForm
+} = require('../controllers/viewsController');
 
 const router = express.Router();
 
 router.get('/', getOverview);
-
 router.get('/tour/:slug', getTour);
+router.get('/login', getLoginForm);
 
 module.exports = router;
