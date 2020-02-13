a0dd166fdd791358b907646682938f6e98a17195
Jorge Contreras <jorge.contreras789@gmail.com>
2020-02-13 07:52:48 -0500

    Updated login.js

--- public/js/login.js
+++ public/js/login.js
@@ -19,7 +19,7 @@ export const login = async (email, password) => {
       }, 1500);
     }
   } catch (err) {
-    showAler('error', err.response.data.message);
+    showAlert('error', err.response.data.message);
   }
 };
 
